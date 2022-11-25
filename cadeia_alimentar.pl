12597626ed7060f981d8890e7c8b8d81450da024
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-11-25 16:03:10 -0300

    fatos de um cadeia alimentar

---
+++ cadeia_alimentar.pl
@@ -0,0 +1,19 @@
+animal(urso).
+animal(peixe).
+animal(lince).
+animal(raposa).
+animal(coelho).
+animal(veado).
+animal(guaxinim).
+planta(alga).
+planta(grama).
+come(urso, peixe).
+come(lince, veado).
+come(urso, raposa).
+come(urso, veado).
+come(peixe, alga).
+come(guaxinim, peixe).
+come(raposa, coelho).
+come(coelho, grama).
+come(veado, grama).
+come(urso, guaxinim).
